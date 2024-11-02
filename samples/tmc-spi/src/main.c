7dd1edee04b1a9f8ebe05ce3dd27a022361aaf18
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2024-11-02 23:51:37 +0100

    port 5160 spi to stepper API

--- samples/tmc-spi/src/main.c
+++ samples/tmc-spi/src/main.c
@@ -7,15 +7,16 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/stepper.h>
 #include <zephyr/sys/util.h>
 
-#include "tmc5160.h"
-
 #define SLEEP_TIME_MS   100
+#define SPEED_STEP_S  	200
 
 uint8_t toggle, toggle_old;
 
 const struct device *tmc0 = DEVICE_DT_GET( DT_ALIAS(tmc0) );
+const struct device *mot0 = DEVICE_DT_GET( DT_ALIAS(motor0) );
 struct gpio_dt_spec led = GPIO_DT_SPEC_GET( DT_ALIAS(led0), gpios);
 
 #define SW0_NODE	DT_ALIAS(sw0)
@@ -30,7 +31,7 @@ void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t
 	toggle = !toggle;
 }
 
-void main(void) {
+int main(void) {
 
 	int ret;
 
@@ -38,21 +39,21 @@ void main(void) {
 	if (!device_is_ready(button.port)) {
 		printk("Error: button device %s is not ready\n",
 		       button.port->name);
-		return;
+		return -ENODEV;
 	}
 
 	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
 	if (ret != 0) {
 		printk("Error %d: failed to configure %s pin %d\n",
 		       ret, button.port->name, button.pin);
-		return;
+		return -ENODEV;
 	}
 
 	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
 	if (ret != 0) {
 		printk("Error %d: failed to configure interrupt on %s pin %d\n",
 			ret, button.port->name, button.pin);
-		return;
+		return -ENODEV;
 	}
 
 	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
@@ -60,18 +61,23 @@ void main(void) {
 	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
 
 
+
 	// check tmc
-	if (!device_is_ready(tmc0)) {
+	if (!device_is_ready(mot0)) {
 		printk("TMC device not ready\n");
-		return;
+		return -ENODEV;
 	}
+	stepper_enable(mot0, true);
+	stepper_move(mot0, 200);
+	//stepper_enable_constant_velocity_mode(mot0, STEPPER_DIRECTION_POSITIVE, SPEED_STEP_S);
+
 
 	// config LED
 	if( !device_is_ready(led.port) ) {
-		return;
+		return -ENODEV;
 	}
 	if( gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0 ) {
-		return;
+		return -ENODEV;
 	}
 	gpio_pin_set_dt(&led, 0);
 
@@ -80,24 +86,34 @@ void main(void) {
 
 	while (1) {
 
-		if(toggle!=toggle_old) {
-			// set LED
-			if( gpio_pin_set_dt(&led, toggle) < 0 ) {
-				return;
-			}
+		// set LED
+		if( gpio_pin_toggle_dt(&led) < 0 ) {
+			return -EIO;
+		}
+
+		/*if(toggle!=toggle_old) {
 
 			count++;
+
 			// start/stop motor
+			if(count == 2){
+				stepper_enable_constant_velocity_mode(mot0, STEPPER_DIRECTION_POSITIVE, SPEED_STEP_S);
+			} else if(count == 1) {
+				stepper_enable_constant_velocity_mode(mot0, STEPPER_DIRECTION_NEGATIVE, SPEED_STEP_S);
+			}
+
 			if(count == 2)
 				count=-1;
 
-			tmc_run(tmc0, 0, count*60, 0);
+			//tmc_run(tmc0, 0, count*60, 0);
+			// test ustep=1
+			stepper_move(mot0, 200);
 
 			toggle_old = toggle;
-		}
-
+		}*/
 
 		k_msleep(SLEEP_TIME_MS);
-
 	}
+
+	return 0;
 }
